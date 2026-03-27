88262ea1fd85f1f7c71fd046f32e4d0eb4e8b2db
selva <selva@deccanclub.com>
2026-03-27 16:24:04 +0530

    Add gh-pages dependency and update deploy script in package.json; enhance Vite config for chunk management

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/image-annotator/',
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('konva')) {
+            return 'konva-chunk';
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
